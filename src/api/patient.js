4584e278a78a2648d3a5cb4d85a939b7237f51bb
fionabeatricewong <fionabeatricewong@gmail.com>
2020-09-17 10:13:48 -0400

    Added axios call for create

--- src/api/patient.js
+++ src/api/patient.js
@@ -21,3 +21,24 @@ export const index = (patient, user) => {
     }
   })
 }
+
+export const patientCreate = (patient, user) => {
+  return axios({
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    url: apiUrl + '/patients',
+    method: 'POST',
+    data: {
+      patient: {
+        firstName: patient.firstName,
+        middleName: patient.middleName,
+        lastName: patient.lastName,
+        dob: patient.dob,
+        gender: patient.gender,
+        phone: patient.phone,
+        email: patient.email
+      }
+    }
+  })
+}
