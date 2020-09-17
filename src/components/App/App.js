0d2077fa75e2d8c1b492e660565f8180711455bc
fionabeatricewong <fionabeatricewong@gmail.com>
2020-09-16 20:13:38 -0400

    Imported Patients component and added AuthenticatedRoute for Patients

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,7 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
+import Patients from '../routes/Patients'
 
 class App extends Component {
   constructor () {
@@ -57,6 +58,9 @@ class App extends Component {
           )} />
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/patients' render={() => (
+            <Patients msgAlert={this.msgAlert} user={user} />
           )} />
         </main>
       </Fragment>
