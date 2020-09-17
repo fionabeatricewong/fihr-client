aaac03849a941732f0abe38ef5043491e84f583f
fionabeatricewong <fionabeatricewong@gmail.com>
2020-09-16 20:24:25 -0400

    Added patient index success and failure messages

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -5,5 +5,7 @@ export default {
   signInFailure: 'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
+  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
+  patientIndexSuccess: 'You can search for one.',
+  patientIndexFailure: 'Do you have patients?'
 }
