f2ed7c8d0d0d8614ecff7cf4215e8e6dde3cd639
fionabeatricewong <fionabeatricewong@gmail.com>
2020-09-16 20:37:06 -0400

    Successful index

---
+++ src/api/patient.js
@@ -0,0 +1,23 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const index = (patient, user) => {
+  return axios({
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    url: apiUrl + '/patients/',
+    method: 'GET',
+    data: {
+      patient: {
+        firstName: patient.firstName,
+        middleName: patient.middleName,
+        lastName: patient.lastName,
+        dob: patient.dob,
+        gender: patient.gender,
+        phone: patient.phone,
+        email: patient.email
+      }
+    }
+  })
+}
---
+++ src/components/routes/Patients.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { index } from '../../api/patient'
+import messages from '../AutoDismissAlert/messages'
+// import Layout from '../../components/Layout'
+
+class Patients extends Component {
+  constructor () {
+    super()
+    this.state = {
+      patients: []
+    }
+  }
+
+  componentDidMount () {
+    const { user, msgAlert } = this.props
+    index(this.state, user)
+      .then(res => this.setState({ patients: res.data.patients }))
+      .then(() => msgAlert({
+        heading: 'Here are all your patients!',
+        message: messages.patientIndexSuccess,
+        variant: 'success'
+      }))
+      // .then(() => history.push('/'))
+      .catch(error => {
+        msgAlert({
+          heading: 'Can\'t see your patients.' + error.message,
+          message: messages.patientIndexFailure,
+          variant: 'danger'
+        })
+      })
+  }
+
+  render () {
+  // const { name, quantity, price } = this.state
+    const patients = this.state.patients.map(patient => (
+      <tr key={patient._id}>
+        <td><Link to={`/patients/${patient._id}`}>{patient.name}</Link></td>
+      </tr>
+    ))
+    return (
+      // <Layout>
+      <table>
+        <tbody>
+          {patients}
+        </tbody>
+      </table>
+      // </Layout>
+    )
+  }
+}
+
+export default withRouter(Patients)
